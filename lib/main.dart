0aafe2f79130af53d2c27e1e894447d935907dc5
seongukHeo <heoseonguk3@gmail.com>
2024-05-12 12:49:00 +0900

    feat : Dialog 활용하기

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+      home: MyApp())
+  );
 }
 
 
@@ -18,42 +20,75 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
+    return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: (){
+            showDialog(
+                context: context,
+                builder: (context){
+                  return DialogTest(state : a);
+                }
+            );
+          },
+        ),
         appBar: AppBar(title: Text("연락처앱"),),
         body: ListView.builder(
           itemCount: 3,
             itemBuilder: (c, i){
             return ListTile(
-              leading: Text(a[i].toString()),
+              leading: Icon(Icons.account_circle),
               title: Text(name[i]),
-              trailing: ElevatedButton(
-                child: Text("좋아요"),
-                onPressed: (){
-                  setState(() {
-                    a[i]++;
-                  });
-                },
-              ),
             );
             }
         )
+      );
+  }
+}
+
+
+
+class DialogTest extends StatelessWidget {
+  DialogTest({Key? key, this.state}) : super(key: key);
+  var state;
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: Container(
+        width: 220, height: 195, padding: EdgeInsets.all(25),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              child: Text("Contact", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20) ),
+            ),
+            SizedBox(
+              child: TextField(),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                      onPressed: (){
+                        Navigator.pop(context);
+                      },
+                      child: Text("Cancel")
+                  ),
+                  TextButton(
+                      onPressed: (){},
+                      child: Text(state.toString())
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
 }
 
-
-
-//그니까 이건 안드로이드에서 함수 나누기? 뭐 그런 셈이지
-class ShopItem extends StatelessWidget {
-  const ShopItem({super.key});
-
-  @override
-  build(context) {
-    return SizedBox(
-      child: Text("gk"),
-    );
-  }
-}
 
